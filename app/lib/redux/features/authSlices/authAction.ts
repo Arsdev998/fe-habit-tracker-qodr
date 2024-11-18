982fd1635077a611b980bf802cfc7bc5806ee86e
Arsdev998 <arsdev998@gmail.com>
2024-11-19 06:36:59 +0700

    test href after login

--- app/lib/redux/features/authSlices/authAction.ts
+++ app/lib/redux/features/authSlices/authAction.ts
@@ -1,48 +1,51 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axiosInstance from "../../../AxiosInstance";
 
-
 export const login = createAsyncThunk(
   "auth/login",
-  async (credentials: { name: string , password: string },{rejectWithValue}) => {
+  async (
+    credentials: { name: string; password: string },
+    { rejectWithValue }
+  ) => {
     try {
-      const response = await axiosInstance.post("/auth/login",
-        credentials
-      );
+      const response = await axiosInstance.post("/auth/login", credentials);
+      window.location.href = "/";
       return response.data;
-    } catch (err:any) {
-      if(!err?.response){
-        throw err
+    } catch (err: any) {
+      if (!err?.response) {
+        throw err;
       }
       return rejectWithValue(err?.response?.data?.message);
     }
   }
 );
 
-export const getStatus = createAsyncThunk("auth/getStatus",async (_, {rejectWithValue}) => {
-  try {
-    const response = await axiosInstance.get("/auth/status")
-    return response.data
-  } catch (err:any) {
-    if(!err?.response){
-      throw err
-    }
-    return rejectWithValue(err?.response?.data?.message);
-  }
-})
-
-
-export const logout = createAsyncThunk(
-  "auth/logout",
-  async (_, {rejectWithValue}) => {
+export const getStatus = createAsyncThunk(
+  "auth/getStatus",
+  async (_, { rejectWithValue }) => {
     try {
-      const response = await axiosInstance.post("/auth/logout")
-      return response.data
-    } catch (err:any) {
-      if(!err?.response){
-        throw err
+      const response = await axiosInstance.get("/auth/status");
+      return response.data;
+    } catch (err: any) {
+      if (!err?.response) {
+        throw err;
       }
       return rejectWithValue(err?.response?.data?.message);
     }
   }
-)
+);
+
+export const logout = createAsyncThunk(
+  "auth/logout",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axiosInstance.post("/auth/logout");
+      return response.data;
+    } catch (err: any) {
+      if (!err?.response) {
+        throw err;
+      }
+      return rejectWithValue(err?.response?.data?.message);
+    }
+  }
+);
