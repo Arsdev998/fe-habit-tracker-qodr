a21989a6c9a0fbd5ab910dd14acccc03fdf8f6c2
Arsdev998 <arsdev998@gmail.com>
2024-11-19 07:51:16 +0700

    test fix cookie

--- app/lib/AxiosInstance.ts
+++ app/lib/AxiosInstance.ts
@@ -3,7 +3,10 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_DB_HOST,
-  withCredentials: true
+  withCredentials: true,
+  headers:{
+    'Content-Type': 'application/json'
+  }
 });
 
 export default axiosInstance;
