7a645bdfa4a9b98bf17b8b701e177295662c0e2e
Arsdev998 <arsdev998@gmail.com>
2024-11-25 10:03:50 +0700

    fix error

--- app/components/admin/user/TahfidzUserPage.tsx
+++ app/components/admin/user/TahfidzUserPage.tsx
@@ -6,15 +6,38 @@ import Murajaah from "../../dashboard/al-quran-page/Murajaah";
 import Ziyadah from "../../dashboard/al-quran-page/Ziyadah";
 import Tilawah from "../../dashboard/al-quran-page/Tilawah";
 import { useGetAllMonthHabitsQuery } from "@/app/lib/redux/api/habitApi";
+import { useState } from "react";
 
 const TahfidzUserPage = () => {
   const { userId } = useParams();
   const isValidId = typeof userId === "string" ? userId : "";
+  const [selectedMonthId, setSelectedMonthId] = useState<string | null>(null);
 
   const { data: monthData, isLoading: monthLoading } =
     useGetAllMonthHabitsQuery();
+
   return (
     <div>
+      {monthLoading ? (
+        <Skeleton className="w-full h-[50px] mb-4 bg-black/20" />
+      ) : (
+        <Tabs defaultValue={monthData?.[0]?.id ?? ""} className="w-full mb-4">
+          <TabsList className="flex p-0 bg-transparent justify-center">
+            {monthData?.map((month) => (
+              <TabsTrigger
+                key={month.id}
+                value={month.id}
+                className={`px-4 py-2 ${
+                  selectedMonthId === month.id ? "bg-gray-200" : ""
+                }`}
+                onClick={() => setSelectedMonthId(month.id)}
+              >
+                {month.name}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+        </Tabs>
+      )}
       <Tabs defaultValue="murajaah" className="border-2">
         <TabsList>
           <TabsTrigger value="murajaah" className="border-2">
@@ -32,13 +55,13 @@ const TahfidzUserPage = () => {
         ) : (
           <div>
             <TabsContent value="murajaah">
-              <Murajaah monthData={monthData ?? []} userId={isValidId} />
+              <Murajaah monthId="" userId={isValidId} />
             </TabsContent>
             <TabsContent value="ziyadah">
-              <Ziyadah monthData={monthData ?? []} userId={isValidId} />
+              <Ziyadah monthId="" userId={isValidId} />
             </TabsContent>
             <TabsContent value="tilawah">
-              <Tilawah monthData={monthData ?? []} userId={isValidId} />
+              <Tilawah monthId="" userId={isValidId} />
             </TabsContent>
           </div>
         )}
